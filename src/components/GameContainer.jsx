6ae6bfbb2c375f155dba8eb1e7d46c1937f5b56b
Leo Kirasic <kirasicleo@gmail.com>
2022-03-10 18:28:21 +0100

    Create contextMenu

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { ControlledMenu, MenuItem, useMenuState } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+
 function GameContainer() {
   const background = require('../assets/background.jpg');
 
-  return <img src={background} />;
+  const [menuProps, toggleMenu] = useMenuState();
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+
+  return (
+    <div
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setAnchorPoint({ x: e.clientX, y: e.clientY });
+        toggleMenu(true);
+      }}
+    >
+      <img src={background} />;
+      <ControlledMenu
+        {...menuProps}
+        anchorPoint={anchorPoint}
+        onClose={() => toggleMenu(false)}
+      >
+        <MenuItem>Waldo</MenuItem>
+        <MenuItem>Odlaw</MenuItem>
+        <MenuItem>Wizard</MenuItem>
+      </ControlledMenu>
+    </div>
+  );
 }
 
 export default GameContainer;
