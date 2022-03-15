9faea5fe816e4394ea5f928986d49732919a3e75
Leo Kirasic <kirasicleo@gmail.com>
2022-03-15 19:58:49 +0100

    Change header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Header() {
-  const title = `Where's Waldo`;
+  const title = `Where's`;
 
-  return <div>{title}</div>;
+  return (
+    <div className="flex">
+      <div className="text-2xl font-bold text-white mr-1">{title}</div>
+      <div className="text-2xl font-bold text-white hover:text-blue">Waldo</div>
+    </div>
+  );
 }
 
 export default Header;
