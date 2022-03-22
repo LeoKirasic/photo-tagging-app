118c4c96fef663963bfc14aca44623fda2c81c3c
Leo Kirasic <kirasicleo@gmail.com>
2022-03-22 16:55:22 +0100

    Add addCharacter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import {React, useState} from 'react';
 import GameContainer from './components/GameContainer';
 import Header from './components/Header';
+
 function App() {
+
+  const [clickedCharacters, setClickedCharacters] = useState([]);
+
+  function addCharacter(id) {
+    setClickedCharacters(clickedCharacters => [...clickedCharacters, id])
+  }
+
+
   return (
     <div className="flex justify-center flex-col items-center ">
       <Header></Header>
-      <GameContainer className=""></GameContainer>
+      <GameContainer clickedCharacters={clickedCharacters} addCharacter={addCharacter}></GameContainer>
     </div>
   );
 }
