05646fbdbfe136fd56959c23909c4f3f3c8db121
Leo Kirasic <kirasicleo@gmail.com>
2022-03-22 19:18:16 +0100

    Switch console.logs with notifications

--- src/helpers/menuItemClickHandler.js
+++ src/helpers/menuItemClickHandler.js
@@ -1,11 +1,17 @@
 import checkCoordinates from "./checkCoordinates";
 import characterAlreadyClicked from "./characterAlreadyClicked";
+import { showError, showSuccess } from "./notifications";
+
 function menuItemClickHandler(id, x, y, clickedCharacters, addCharacter) {
     if(checkCoordinates(id, x, y) === true && characterAlreadyClicked(id, clickedCharacters) === false) {
         addCharacter(id)
-        console.log('all good');
+        showSuccess();
     } else {
-        console.log('not all good');
+        if(characterAlreadyClicked(id, clickedCharacters) === true) {
+            showError('Character Already Found!');
+        } else {
+            showError('Wrong Character!');
+        }
     }
 }
 export default menuItemClickHandler;
