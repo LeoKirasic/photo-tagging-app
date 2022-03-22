44b9ea122cfd95d3e96f1abca26cbb7455c1900c
Leo Kirasic <kirasicleo@gmail.com>
2022-03-22 16:54:04 +0100

    Create checkCoordinates function

--- src/helpers/checkCoordinates.js
+++ src/helpers/checkCoordinates.js
@@ -5,7 +5,7 @@ function checkCoordinates(id, x, y) {
   const index = arr.findIndex((element) => element.id === id);
   if (x >= arr[index].x - 15 && x <= arr[index].x + 15) {
     if (y >= arr[index].y - 15 && y <= arr[index].y + 15) {
-      return id
+      return true;
     }
   }
 }
