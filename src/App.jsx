fe874ea29722a8c027d597b95e7ad644c7a9985d
Leo Kirasic <kirasicleo@gmail.com>
2022-03-23 13:05:48 +0100

    Add viewCharactersModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import GameContainer from './components/GameContainer';
 import Header from './components/Header';
-
+import ViewCharactersModal from './components/ViewCharactersModal';
 function App() {
-
   const [clickedCharacters, setClickedCharacters] = useState([]);
 
   function addCharacter(id) {
-    setClickedCharacters(clickedCharacters => [...clickedCharacters, id])
+    setClickedCharacters((clickedCharacters) => [...clickedCharacters, id]);
   }
-
 
   return (
     <div className="flex justify-center flex-col items-center ">
       <Header></Header>
-      <GameContainer clickedCharacters={clickedCharacters} addCharacter={addCharacter}></GameContainer>
+      <ViewCharactersModal></ViewCharactersModal>
+
+      <GameContainer
+        clickedCharacters={clickedCharacters}
+        addCharacter={addCharacter}
+      ></GameContainer>
     </div>
   );
 }
