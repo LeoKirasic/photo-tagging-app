cdefa4012bade36de388447f1f11907c84b72a24
Leo Kirasic <kirasicleo@gmail.com>
2022-03-23 17:35:46 +0100

    Declare setFinished

--- src/helpers/menuItemClickHandler.js
+++ src/helpers/menuItemClickHandler.js
@@ -2,13 +2,23 @@ import checkCoordinates from './checkCoordinates';
 import characterAlreadyClicked from './characterAlreadyClicked';
 import { showError, showSuccess } from './notifications';
 
-function menuItemClickHandler(id, x, y, clickedCharacters, addCharacter) {
+function menuItemClickHandler(
+  id,
+  x,
+  y,
+  clickedCharacters,
+  addCharacter,
+  setFinished
+) {
   if (
     checkCoordinates(id, x, y) === true &&
     characterAlreadyClicked(id, clickedCharacters) === false
   ) {
     addCharacter(id);
     showSuccess();
+    if (clickedCharacters.length === 2) {
+      setFinished();
+    }
   } else {
     if (characterAlreadyClicked(id, clickedCharacters) === true) {
       showError('Character Already Found!');
