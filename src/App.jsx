53afdc6253d571fb451477b85f4b06b6f2b5aef4
Leo Kirasic <kirasicleo@gmail.com>
2022-03-23 17:31:59 +0100

    Add setFinished function

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import Header from './components/Header';
 import ViewCharactersModal from './components/ViewCharactersModal';
 function App() {
   const [clickedCharacters, setClickedCharacters] = useState([]);
+  const [isFinished, setIsFinished] = useState(false);
 
   function addCharacter(id) {
     setClickedCharacters((clickedCharacters) => [...clickedCharacters, id]);
+  }
+
+  function setFinished() {
+    setIsFinished(true);
   }
 
   return (
     <div className="flex justify-center flex-col items-center ">
       <Header></Header>
       <ViewCharactersModal></ViewCharactersModal>
-
+      <Form isFinished={isFinished}></Form>
       <GameContainer
         clickedCharacters={clickedCharacters}
         addCharacter={addCharacter}
+        setFinished={setFinished}
       ></GameContainer>
     </div>
   );
