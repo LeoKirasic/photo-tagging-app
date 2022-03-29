69cf327d55e32cb7685f9b9510e62f02190ec320
Leo Kirasic <kirasicleo@gmail.com>
2022-03-29 18:53:42 +0200

    Change border color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   const title = `Where's`;
 
   return (
-    <div className="flex">
+    <div className="flex border-x-black">
       <div className="text-2xl font-bold text-white mr-1">{title}</div>
       <div className="text-2xl font-bold text-white hover:text-blue">Waldo</div>
     </div>
