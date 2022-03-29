f759f4fb28f9360e2a9dc8d532549ee24b57543b
Leo Kirasic <kirasicleo@gmail.com>
2022-03-29 15:26:59 +0200

    Create Leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import getLeaderboard from '../helpers/getLeaderboard';
+import LeaderboardCard from './LeaderboardCard';
+import { useState, useEffect } from 'react';
+
 function Leaderboard() {
-  const leaderboard = getLeaderboard();
-  console.log(leaderboard);
-  return <div className="text-white">Leaderboard</div>;
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const promise = getLeaderboard();
+    promise.then((data) => {
+      setLeaderboard(data);
+      setIsLoaded(true);
+    });
+    (error) => {
+      setIsLoaded(true);
+      setError(error);
+    };
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {leaderboard.map((item) => {
+          return (
+            <LeaderboardCard key={item.id} name={item.name} time={item.time} />
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default Leaderboard;
