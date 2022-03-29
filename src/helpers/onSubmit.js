3ffb85830f88209d6e193052be5ee60b3356edb7
Leo Kirasic <kirasicleo@gmail.com>
2022-03-29 15:28:14 +0200

    Remove onSubmit

--- src/helpers/onSubmit.js
+++
@@ -1,9 +0,0 @@
-import { app, db } from '../firebase';
-import { setDoc, doc } from 'firebase/firestore';
-import uniqid from 'uniqid';
-
-async function onSubmit(data) {
-  await setDoc(doc(db, 'leaderboards', uniqid()), data);
-}
-
-export default onSubmit;
