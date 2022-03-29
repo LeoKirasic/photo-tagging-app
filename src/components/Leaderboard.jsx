7995e996fcac0e7ad7ecd50f6a31918aaf37fd80
Leo Kirasic <kirasicleo@gmail.com>
2022-03-29 18:53:01 +0200

    Refactor Leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
 import getLeaderboard from '../helpers/getLeaderboard';
 import LeaderboardCard from './LeaderboardCard';
+import LeaderboardList from './LeaderboardList';
 import { useState, useEffect } from 'react';
 
 function Leaderboard() {
-  const [leaderboard, setLeaderboard] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [leaderboard, setLeaderboard] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(async () => {
-    const promise = await getLeaderboard();
-    promise.then((data) => {
+    try {
+      const data = await getLeaderboard();
       setLeaderboard(data);
-      setIsLoaded(true);
-    });
-    (error) => {
-      setIsLoaded(true);
+    } catch (error) {
       setError(error);
-    };
+    }
   }, []);
 
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
+  } else if (!leaderboard) {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {leaderboard.map((item) => {
-          return (
-            <LeaderboardCard key={item.id} name={item.name} time={item.time} />
-          );
-        })}
-      </ul>
+      <div className="flex items-center flex-col text-white bg-slate-900">
+        <h2 className="border-b border-slate-600 font-bold text-xl">
+          Leaderboard
+        </h2>
+        <LeaderboardList items={leaderboard}></LeaderboardList>
+      </div>
     );
   }
 }
