79c250f5fddd1719c67ee6decfb6932d72f1b7cb
Leo Kirasic <kirasicleo@gmail.com>
2022-03-30 17:10:36 +0200

    Remove console.log

--- src/components/LeaderboardCard.jsx
+++ src/components/LeaderboardCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 function LeaderboardCard(props) {
-  console.log('ello', props);
   return (
     <li className="text-white flex border-b border-slate-600">
       <div className=" pr-2 text-blue">{props.name}</div>
