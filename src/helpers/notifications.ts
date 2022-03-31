eff4426a7232a123b455f91454a3e84fd3f05bf5
Leo Kirasic <kirasicleo@gmail.com>
2022-03-31 17:37:12 +0200

    Refactor in typescript

---
+++ src/helpers/notifications.ts
@@ -0,0 +1,17 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
+
+const showError = (message: string) => {
+  toast.error(message, {
+    theme: 'dark',
+  });
+};
+const showSuccess = () => {
+  toast.success('Success!', {
+    theme: 'dark',
+  });
+};
+
+export { showError, showSuccess };
